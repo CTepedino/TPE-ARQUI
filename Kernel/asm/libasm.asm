440d842898e8cf3a83cac2498b5076aaa2505881
CTepedino <108553316+CTepedino@users.noreply.github.com>
2023-05-26 19:24:22 -0300

    funciones rtc

--- Kernel/asm/libasm.asm
+++ Kernel/asm/libasm.asm
@@ -1,5 +1,9 @@
 GLOBAL cpuVendor
 
+GLOBAL getHour
+GLOBAL getMin
+GLOBAL getSec
+
 section .text
 	
 cpuVendor:
@@ -25,3 +29,26 @@ cpuVendor:
 	mov rsp, rbp
 	pop rbp
 	ret
+
+%macro getTime 1
+	push rbp
+        mov rbp, rsp
+
+        mov al, %1
+        out 70h, al
+        in al, 71h
+
+        mov rsp, rbp
+        pop rbp
+        ret
+%endmacro
+
+
+getHour:
+    getTime 4
+
+getMin:
+    getTime 2
+
+getSec:
+    getTime 0
