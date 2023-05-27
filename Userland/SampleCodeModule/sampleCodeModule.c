cdafb3acd24edaab1e4ae4f369a6bee62f21447b
Luca <lbloise@itba.edu.ar>
2023-05-27 16:37:55 -0300

    Meto todo en sampleCodeModule

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -1,23 +1,7 @@
-#include "../libc.h"
-
-/* sampleCodeModule.c */
-
-char * v = (char*)0xB8000 + 79 * 2;
-
-static int var1 = 0;
-static int var2 = 0;
+#include <libc.h>
 
 
 int main() {
-	//All the following code may be removed 
-	//*v = 'X';
-	//*(v+1) = 0x74;
-
-	//Test if BSS is properly set up
-	//if (var1 == 0 && var2 == 0)
-	//	return 0xDEADC0DE;
-
-	//return 0xDEADBEEF;
 	putChar('A');
 	//char *c;
 	//getChar(c);
